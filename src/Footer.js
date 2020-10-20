88425a93ec5631587af4dcf576250ecfb32fd85b
Utkarsh Singh <utkarsh.singh0609@gmail.com>
2020-10-20 16:45:19 -0500

    minor

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,9 @@ import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 import RepeatIcon from '@material-ui/icons/Repeat';
-
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import { Grid, Slider } from "@material-ui/core";
 function Footer() {
     return (
         <div className="footer">
@@ -22,7 +24,17 @@ function Footer() {
             </div>
 
             <div className="footer__right">
-                <p>Volume Controls</p>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
             </div>
         </div>
     );
